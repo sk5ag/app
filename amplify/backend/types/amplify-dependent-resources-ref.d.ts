2da74ae38505fc00bbb87040e7e77a02f99d65d3
Kardo <sk5ag.android@gmail.com>
2022-03-12 13:12:26 +0300

    AuthPostConfirmationToDynamoDb

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,6 +9,23 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string",
             "CreatedSNSRole": "string"
+        },
+        "userPoolGroups": {
+            "AdminGroupRole": "string"
+        }
+    },
+    "function": {
+        "app01f72b4b0ePostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "app01": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
         }
     }
 }
